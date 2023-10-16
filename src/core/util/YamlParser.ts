0ded38e078de0ef81ad8adf835b959362efafeaa
Ben Champagne <donkeykong797@gmail.com>
2023-10-16 19:22:09 -0400

    AAAAAAAAAAAAA

--- src/core/util/YamlParser.ts
+++ src/core/util/YamlParser.ts
@@ -1,19 +1,78 @@
-import { Notice, TFile, parseYaml } from "obsidian";
+import { Notice } from "obsidian";
+import { parse } from "yaml";
 
-export interface YamlPair<K, V> { key: K, value: V}
-export interface YamlObject extends Object {
-    arrays:  Array<YamlPair<string, Array<any>>>;
-    objects: Array<YamlPair<string, YamlObject>>;
-    strings: Array<YamlPair<string, string>>;
-    numbers: Array<YamlPair<string, number>>;
+export interface PropPair<K, V> { key: K, value: V}
+export interface PropObject {
+    arrays:  Array<PropPair<string, Array<PropObject>[] | string[] | number[]>>;
+    objects: Array<PropPair<string, PropObject>>;
+    strings: Array<PropPair<string, string>>;
+    numbers: Array<PropPair<string, number>>;
 }
 
 export default class YamlParser {
-    public static parseFromString(data: string): YamlObject | undefined {
-        let obj: YamlObject | undefined;
+    public static parseFromString(data: string): PropObject {
+        let yaml: PropObject = {
+            arrays: [],
+            objects: [],
+            strings: [],
+            numbers: []
+        };
+        
+        const sep: string = this.separateFromString(data)
+        const obj: object = parse(sep)
 
-        new Notice(`Parsing:\n${JSON.stringify(parseYaml(data))}`, 0)
+        const mapToYamlObject = (yamlObj: PropObject, entries: [string, any][]): PropObject => {
+            for (const [key, value] of entries) {
+                switch (typeof(value)) {
+                    case "string": {
+                        yamlObj.strings.push({ key, value })
+                    } break; 
+                    case "number": {
+                        yamlObj.numbers.push({ key, value })
+                    } break;
+                    case "object": {
+                        if (Array.isArray(value)) {
+                            const arr: any[] = []
+                            value.forEach(v => {
+                                switch(typeof(v)) {
+                                    case "string": {
+                                        arr.push(v)
+                                    } break;
+                                    case "number": {
+                                        arr.push(v)
+                                    } break;
+                                    case "object": {
+                                        if (Array.isArray(v)) {
+                                            // Implement
+                                        } else {
+                                            arr.push(mapToYamlObject({arrays: [], numbers: [], objects: [], strings: []} as PropObject, Object.entries(v)))
+                                        }
+                                    } break;
+                                }
+                            })
+                            yamlObj.arrays.push({key, value: arr})
+                        } else {
+                            yamlObj.objects.push({key: key, value: mapToYamlObject(yamlObj, Object.entries(value))})
+                        }
+                    } break;
+                }
+            }
+            return yamlObj;
+        }
+        mapToYamlObject(yaml, Object.entries(obj))
 
-        return obj;
+        console.log("" + JSON.stringify(yaml))
+
+        return yaml;
+    }
+
+    /**
+     * Parses an entire file and finds the text surrounded by three dashes
+     * Then it parses it into a json file
+     */
+    public static separateFromString(data: string): string {
+        let str: string = data; 
+        str = new RegExp(/(?<=\-\-\-\n)[\s\S]+(?=\-\-\-)/).exec(str)?.[0] ?? str
+        return str;
     }
 }
--- src/handlers/EventHandler.ts
+++ src/handlers/EventHandler.ts
@@ -16,13 +16,13 @@ export default class EventHandler {
     async setup(): Promise<void> {
         // anytime a file is opened, check its `class` property, and update it accordingly
         this.plugin.registerEvent(this.plugin.app.vault.on("modify", async (classFile: TFile) => {
-            new Notice(`"modify" Event: ${classFile.name}`)
             // anytime a `settings.classes_folder` file is changed, 
             // go through all files and refactor their `class` variable if it was the same as the files previous title
             if (classFile.parent?.path == this.settings.classesFolder) {
                 this.plugin.app.vault.getFiles().forEach(async (file: TFile) => {
                     const data = await this.plugin.app.vault.read(file)
                     const yaml = YamlParser.parseFromString(data)
+                    new Notice(JSON.stringify(yaml), 0)
 
                     // If the file has the `class` property
                     const className = yaml?.strings.filter((pair) => pair.key != 'class')?.at(-1)
